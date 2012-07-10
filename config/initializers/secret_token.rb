209fc51108f82fe01349d3e867a037940fa4c674
hcurotta <hcurotta@gmail.com>
2012-07-10 14:27:33 -0500

    Initial proper commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwingbuyApp::Application.config.secret_token = '3bba473ba7a338661ffe4a19a1b42ea8f4673aeef2135dd26fa1a9a8f1f4658f1774cc403a40a349d8f4f831b514d4e0585a62d149e5dd6336781b70a5aba0e4'
